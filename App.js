7e36203ab0121df5e6b48bf623335021842bcf8d
Jonathan <tumjoncrm@gmail.com>
2020-05-26 16:22:35 -0500

    revisar

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {SafeAreaView,ScrollView } from 'react-native';
 import AppNavigator from './src/routes/AppNavigator';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { client } from './src/apollo';
 
 
 
 function App()  {
   console.disableYellowBox = true;
-  return (<AppNavigator/>);
+  return (
+    <ApolloProvider client={client}>
+      <AppNavigator/>
+    </ApolloProvider>
+  );
 };
 
 
