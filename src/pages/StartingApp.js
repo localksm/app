806892629b975687e9188a595fb789cf48b35da3
Luis Acerv <luis@catalyst.com.mx>
2020-06-16 14:44:39 -0500

    Navigate to onboarding

--- src/pages/StartingApp.js
+++ src/pages/StartingApp.js
@@ -1,50 +1,54 @@
-import  React, {useEffect} from 'react';
-import { View, Image, StyleSheet, Text, StatusBar, ActivityIndicator } from 'react-native';
+import React, { useEffect } from 'react';
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  StatusBar,
+  ActivityIndicator,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+const navigateToOnboarding = () => {
+  const navigation = useNavigation();
+  navigation.navigate('Onboarding');
+};
 
+StartingApp = () => {
+    // React does NOT allow to use hooks inside timeouts, hook should be used only on pure functions.
+    navigateToOnboarding();
 
-StartingApp = (props) => {
-
-    useEffect(() => {        
-        setTimeout(()=>{
-            //const navigation = useNavigation();
-            //navigation.navigate('Onboarding');
-            props.navigation.navigate('Auth', { screen: 'Onboarding' })
-        },1000);
-    },[]);
-    
-    
-    return (
-        <View style={ {...styles.container} } >
-             <StatusBar key="bar" backgroundColor="#2D2D2D" barStyle="light-content" />
-             <Image 
-                style={ styles.logo }
-                source={ require('../../assets/logoKSMNoText.png')} />
-            <ActivityIndicator size='large'  color='white' style={ styles.spinner}/>
-            <Text style={styles.text}> Loading ....</Text>
-        </View>
-    )
-}
+  return (
+    <View style={{ ...styles.container }}>
+      <StatusBar key="bar" backgroundColor="#2D2D2D" barStyle="light-content" />
+      <Image
+        style={styles.logo}
+        source={require('../../assets/logoKSMNoText.png')}
+      />
+      <ActivityIndicator size="large" color="white" style={styles.spinner} />
+      <Text style={styles.text}> Loading ....</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2D2D2D'
-    },
-    logo:{
-        width: '60%',
-        height: '40%',   
-        resizeMode: 'contain'     
-    },
-    text:{
-        color:'white'
-    },
-    spinner:{
-        paddingTop: 5,        
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2D2D2D',
+  },
+  logo: {
+    width: '60%',
+    height: '40%',
+    resizeMode: 'contain',
+  },
+  text: {
+    color: 'white',
+  },
+  spinner: {
+    paddingTop: 5,
+  },
 });
 
 export default StartingApp;
