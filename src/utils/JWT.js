f09f169f8356e60e244069aa7a33586643473830
Jonathan <tumjoncrm@gmail.com>
2020-07-23 12:37:10 -0500

    CTSY2020-1443 Store jwt pin in async storge

---
+++ src/utils/JWT.js
@@ -0,0 +1,34 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import jwt from 'react-native-pure-jwt';
+import { ENV_VARS } from './config';
+
+export const storePin = value => {
+  try {
+    jwt
+      .sign(
+        {
+          pin: value,
+          exp: new Date().getTime() + 360000 * 100000, // expiration date, required, in ms, absolute to 1/1/1970
+        }, // body
+        ENV_VARS.JWT_SECRET, // secret
+        {
+          alg: 'HS256',
+        },
+      )
+      .then(async token => {
+        await AsyncStorage.removeItem('pin'); // Remove any previous token before saving
+        await AsyncStorage.setItem('pin', token);
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const getPin = async () => {
+  const response = await AsyncStorage.getItem('pin');
+
+  return response;
+};
