972147e6d4ffebfc777969951d4d4027cb9d8ca5
nestor Ivan Morales <nestor@catalyst.com.mx>
2020-08-11 13:08:45 -0500

    add test to SendSettlementMakerContent

--- __tests__/SendSettlementMaker.test.js
+++ __tests__/SendSettlementMaker.test.js
@@ -7,9 +7,7 @@ import React from 'react';
 import { configure } from 'enzyme';
 import renderer from 'react-test-renderer';
 import Adapter from 'enzyme-adapter-react-16';
-import { fireEvent, render } from 'react-native-testing-library';
-
-import { SendSettlementMakerContent } from '../src/organisms';
+import { SendSettlementMaker } from '../src/pages';
 
 configure({ adapter: new Adapter() });
 
@@ -38,34 +36,6 @@ const mockParams = {
 it('renders correctly', async () => {
   jest.useFakeTimers();
   await renderer.act(() => {
-    renderer.create(<SendSettlementMakerContent />);
+    renderer.create(<SendSettlementMaker />);
   });
-});
-
-it('test report a problem on press', async () => {
-  jest.useFakeTimers();
-  const openHandler = jest.fn();
-
-  const { getByTestId } = render(
-    <SendSettlementMakerContent
-      navigation={{ navigate: () => {} }}
-      route={mockParams}
-    />,
-  );
-  fireEvent.press(getByTestId('link'));
-  expect(openHandler).not.toHaveBeenCalled();
-});
-
-it('test send settlement on press', async () => {
-  jest.useFakeTimers();
-  const openHandler = jest.fn();
-
-  const { getByTestId } = render(
-    <SendSettlementMakerContent
-      navigation={{ navigate: () => {} }}
-      route={mockParams}
-    />,
-  );
-  fireEvent.press(getByTestId('test-btn'));
-  expect(openHandler).not.toHaveBeenCalled();
 });
